e501ca9f817eb82fcd523642baf454117d73ed28
salmanyagaka <salma@buupass.com>
2021-11-03 20:59:38 +0300

    add authentication to create role endpoint

--- foodapi/settings.py
+++ foodapi/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'foodapi.apps.authentication',
-    'foodapi.apps.roles'
 ]
 
 AUTH_USER_MODEL = 'authentication.User'
--- foodapi/urls.py
+++ foodapi/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('foodapi/', include('foodapi.apps.authentication.urls')),
-    path('role/', include('foodapi.apps.roles.urls')),
 
 ]
