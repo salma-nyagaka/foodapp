185b3cc1f8ee90d460c730eb73929c9d88b358d1
salmanyagaka <salma@buupass.com>
2021-11-04 11:42:40 +0300

    add travis config

--- foodapi/settings.py
+++ foodapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = "2v&zps$h!l16%&8zfm$+!8m%h#gu6m2r^rqx0l^a4f$h7gc)j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
